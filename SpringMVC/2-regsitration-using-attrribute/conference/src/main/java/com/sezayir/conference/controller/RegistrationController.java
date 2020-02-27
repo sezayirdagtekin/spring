6f707eab9ff66f48e2bbac53d24c02205da3d6c7
sezayirdagtekin <sezayir@hotmail.com>
2020-02-27 17:36:14 +0300

    use @ModelAttribute-refactor

--- SpringMVC/2-regsitration-using-attrribute/conference/src/main/java/com/sezayir/conference/controller/RegistrationController.java
+++ SpringMVC/2-regsitration-using-attrribute/conference/src/main/java/com/sezayir/conference/controller/RegistrationController.java
@@ -1,16 +1,16 @@
 package com.sezayir.conference.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sezayir.conference.model.Registration;
 
 @Controller
 public class RegistrationController {
 
-	@RequestMapping("/registration")
+	@GetMapping("/registration")
 	public String getRegisteration(@ModelAttribute("registerAtr") Registration registration) {
 		System.out.println("getRegisteration:" + "name=" + registration.getName());
 		return "registration";
